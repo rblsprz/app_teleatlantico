97d80e4be55ff6d090cf6d3a14af4daf2f2ffb90
David Robles <jdrobles0110@gmail.com>
2021-06-28 00:53:23 -0600

    models

---
+++ frontEnd/frontEnd/src/app/comment.model.ts
@@ -0,0 +1,11 @@
+export class Comment{
+
+  id: number=0;
+  description:string;
+  idIssue:number;
+  reportNumber: string;
+  creationDate: Date;
+  creationUser: String;
+  updateDate?: Date;
+  updateUser?: string;
+}
