a2efa376ee62a071a8639860bdb6012230476c8e
David Robles <jdrobles0110@gmail.com>
2021-06-28 00:22:11 -0600

    nav-bar component

--- frontEnd/frontEnd/src/app/app.module.ts
+++ frontEnd/frontEnd/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { IssueComponent } from './issue/issue.component';
 import { IssueDetailsComponent } from './issue-details/issue-details.component';
 import { IssuesComponent } from './issues/issues.component';
 import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { LoginComponent } from './login/login.component';
     IssueComponent,
     IssueDetailsComponent,
     IssuesComponent,
-    LoginComponent
+    LoginComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
