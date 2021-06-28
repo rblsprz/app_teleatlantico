cb82b8791afeb90e1561b2307456dc141aa9869e
David Robles <jdrobles0110@gmail.com>
2021-06-28 01:23:02 -0600

    login update

--- frontEnd/frontEnd/src/app/login/login.component.ts
+++ frontEnd/frontEnd/src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { ClientService } from '../../app/client.service';
+import { Client } from '../../app/client.model';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  client:Client = new Client();
+
+  public loginForm = this.fb.group({
+    email : ['',[ Validators.required, Validators.email]],
+    password : ['',[ Validators.required, Validators.minLength(8)]],
+  });
+
+  constructor(  private fb:FormBuilder,
+                private router:Router,
+                private clientService:ClientService) { }
 
   ngOnInit(): void {
   }
 
+  valueNoValid(value:string) {
+    return this.loginForm.get(value).invalid && this.loginForm.get(value).touched
+  }
+
+  loginUser(){
+
+if(this.loginForm.invalid){
+      let email = document.getElementById("email");
+      let password = document.getElementById("password");
+      email.classList.add("is-invalid");
+      password.classList.add("is-invalid");
+      return;
+}
+
+  this.clientService.login(this.loginForm.value)
+    .subscribe( client =>{
+if(client)   {
+        this.client = client;
+        this.modal('/home','Welcome: '+ (this.client.name).toUpperCase() +' '+(this.client.firstSurname).toUpperCase());
+}
+else{
+        this.modal('','Error!')
+}
+    });
+}
+
+  modal( url:string | '', message:String){
+    let timerInterval
+        Swal.fire({
+        title: message,
+        html: '',
+        timer: 1000,
+        didOpen: () => {
+          Swal.showLoading()
+          timerInterval = setInterval(() => {
+          }, 50)
+        },
+        willClose: () => {
+        clearInterval(timerInterval)
+        }
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            this.router.navigateByUrl(url);
+          }
+        })
+  }
 }
