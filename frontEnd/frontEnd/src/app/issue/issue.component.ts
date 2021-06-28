889fadc8dc4dca3f189de3e7a7d13ed45dfb67fa
David Robles <jdrobles0110@gmail.com>
2021-06-28 01:16:15 -0600

    issue update

--- frontEnd/frontEnd/src/app/issue/issue.component.ts
+++ frontEnd/frontEnd/src/app/issue/issue.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { Client } from '../../app/client.model';
+import { ClientService } from '../../app/client.service';
+import { IssueService } from '../../app/issue.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-issue',
@@ -7,9 +12,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssueComponent implements OnInit {
 
-  constructor() { }
+  client:Client=this.clientService.client;
+
+  public issueForm = this.fb.group({
+    id: 0,
+    idUser: this.client.id,
+    reportNumber : this.newReportNumber(),
+    address : this.client.address,
+    contactPhone : this.client.phone,
+    contactEmail : this.client.email,
+    status : 'Added!',
+    supportUserAsigned : '',
+    service :[0,[ Validators.required]],
+    description : ['',[ Validators.required]],
+    creationDate: new Date(),
+    creationUser: this.client.id
+  });
+
+  constructor(private fb:FormBuilder,
+              private clientService:ClientService,
+              private issueService:IssueService) {
+  }
 
   ngOnInit(): void {
+
+  }
+
+  registerIssue(){
+
+    if(this.issueForm.invalid){
+      return;
+    }
+
+    if( this.issueForm.get("service").value == 0){
+      return;
+    }
+
+    if( (this.issueForm.get("description").value).trim() == ''){
+      return;
+    }
+
+    this.issueService.addIssue(this.issueForm.value)
+    .subscribe( resp =>{
+
+        this.modal('','Success Register!');
+        this.issueForm.get('description').setValue('');
+        this.issueForm.get('service').setValue(0);
+        this.issueForm.get('reportNumber').setValue(this.newReportNumber());
+
+    });
+
+  }
+
+  valueNoValid(value:string) {
+    return this.issueForm.get(value).invalid && this.issueForm.get(value).touched
+  }
+
+  modal( url:string | '', message:String){
+    let timerInterval
+        Swal.fire({
+        title: message,
+        html: '',
+        timer: 1000,
+        didOpen: () => {
+          Swal.showLoading()
+          timerInterval = setInterval(() => {
+          }, 50)
+        },
+        willClose: () => {
+        clearInterval(timerInterval)
+        }
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            //this.router.navigateByUrl(url);
+          }
+        })
+  }
+
+  newReportNumber(){
+    return this.client.id +'-'+ new Date().getTime();
   }
 
 }
