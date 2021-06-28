eee36152c11286d3231e8a507e5f3d96a165c40e
David Robles <jdrobles0110@gmail.com>
2021-06-28 01:20:11 -0600

    issues update

--- frontEnd/frontEnd/src/app/issues/issues.component.ts
+++ frontEnd/frontEnd/src/app/issues/issues.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Issue } from '../../app/issue.model';
+import { IssueService } from '../../app/issue.service';
+import { ClientService } from '../../app/client.service';
+import { Comment } from '../../app/comment.model';
+import { CommentService } from '../../app/comment.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-issues',
@@ -7,9 +14,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssuesComponent implements OnInit {
 
-  constructor() { }
+  issues:Issue[]=[];
+
+  constructor(  private issueService:IssueService,
+                private clientService:ClientService,
+                private commentService:CommentService,
+                private router:Router  ) { }
+
 
   ngOnInit(): void {
+    this.loadIssues();
+  }
+
+  loadIssues(){
+    this.issueService.getIssuesByClientId(this.clientService.client.id)
+    .subscribe( issues => {
+        this.issues=issues;
+    })
+  }
+
+  issueDetail(issueId:number){
+        this.router.navigateByUrl('issues-details/'+issueId);
   }
 
 }
