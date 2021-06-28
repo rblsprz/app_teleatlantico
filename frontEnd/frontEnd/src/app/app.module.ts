57c5945cd48e279e0fb00a670dde2b9d31ae540f
David Robles <jdrobles0110@gmail.com>
2021-06-27 23:53:39 -0600

    issue component

--- frontEnd/frontEnd/src/app/app.module.ts
+++ frontEnd/frontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { IssueComponent } from './issue/issue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    IssueComponent
   ],
   imports: [
     BrowserModule,
